7ea2b5b90e32279ec1b54fa357de18b18d2ac27a
Biotic Pixels <bioticpixels@gmail.com>
2014-05-25 23:55:02 +0100

    Create supporting page

---
+++ javascript/supporting.js
@@ -0,0 +1,34 @@
+//----------
+// Biotic Pixels 26 September 2013 07:10:00
+//----------
+
+(function () {
+
+    "use strict";
+
+    var SupportingScaling = Object.create(DynamicScaling);
+    SupportingScaling.parameters = [
+        [
+            [0, 0],
+            13,
+            [256, 14],
+            ["p, nav, a, ul, li, form",
+                ["fontSize", 1]
+            ],
+            ["#donating",
+                ["top", -15]
+            ],
+            ["#donating form",
+                ["marginTop", 0.5]
+            ],
+            ["#donating input",
+                ["width", 5]
+            ]
+        ]
+    ];
+
+    SupportingScaling.scale();
+
+    window.onresize = function (event) {SupportingScaling.scale(event)}; // JavaScript, it is as beautiful as it is mystical http://stackoverflow.com/questions/183214/javascript-callback-scope
+
+}());
