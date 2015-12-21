41aff2fe365a6ceeda72c6219afdb055550e4cbb
Biotic Pixels <bioticpixels@gmail.com>
2015-12-21 17:05:23 +0000

    Update articles.js

--- javascript/articles.js
+++ javascript/articles.js
@@ -84,8 +84,7 @@
                 ["marginTop", 1]
             ],
             ["code",
-                ["paddingLeft", 0.1],
-                ["paddingRight", 0.1]
+                ["padding", 0.1]
             ],
             ["pre code",
                 ["padding", 1],
