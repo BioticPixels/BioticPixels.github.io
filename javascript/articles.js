1f8f1393b7b00b4a87e0db6be21abc8487c573ee
Biotic Pixels <bioticpixels@gmail.com>
2015-12-21 14:31:02 +0000

    Update articles.js

--- javascript/articles.js
+++ javascript/articles.js
@@ -37,7 +37,7 @@
                 ["fontSize", (1 / 6 * 1)]
             ],
             ["nav",
-                ["marginBottom", 0.2]
+                ["marginBottom", 0.5]
             ]
         ],
         [
