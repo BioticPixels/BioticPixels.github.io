63944200ef8ae7784e37873832ab7b7ec7f73d8e
Biotic Pixels <bioticpixels@gmail.com>
2015-12-21 17:09:05 +0000

    Update articles.js

--- javascript/articles.js
+++ javascript/articles.js
@@ -16,7 +16,7 @@
             ["h1, h2, h3, h4, h5, h6, #bioticPixels",
                 ["fontSize", 1],
                 ["marginTop", 0.3],
-                ["lineHeight", 1]
+                ["lineHeight", 0.5]
             ],
             ["#bioticPixels",
                 ["fontSize", 1],
