2fe128ea49af074765473d46ae53daa68e34cdc8
Biotic Pixels <bioticpixels@gmail.com>
2015-12-21 23:32:53 +0000

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -40,7 +40,8 @@
             13,
             [256, 14],
             ["p, nav, a, ul, li, form, input, h2",
-                ["fontSize", 1]
+                ["fontSize", 1],
+                ["marginTop", 1]
             ]
         ]
     ];
